94fd910f4164e5b470435938d97172a605693e5d
Augusto Yunes <augusto.yunes@accenture.com>
2025-05-27 19:14:59 -0300

    adding userId to answers entity

--- backend/src/controllers/adminAnswer.controller.ts
+++ backend/src/controllers/adminAnswer.controller.ts
@@ -6,7 +6,18 @@ export class AdminAnswerController {
   async createAnswer(req: Request<{}, {}, CreateAnswerInput>, res: Response) {
     try {
       const { method, response, statusCode } = req.body;
-      const answer = AnswerRepository.create({ method, response, statusCode });
+      const userId = parseInt(req.user?.id || '0');
+      console.log(`User ID: ${userId}`);
+      if (!userId) {
+        return res.status(401).json({ status: 'error', error: 'User not authenticated' });
+      }
+
+      const answer = AnswerRepository.create({
+        method,
+        response,
+        statusCode,
+        userId
+      });
       await AnswerRepository.save(answer);
 
       return res.status(201).json({ status: 'success', id: answer.id });
@@ -17,12 +28,12 @@ export class AdminAnswerController {
 
   async getAnswers(req: Request, res: Response) {
     try {
-      const userId  = req.params.userId;
+      const userId = req.params.userId;
       if (!userId) {
         return res.status(400).json({ error: 'UserId ID is required' });
       }
       const id = parseInt(userId);
-      const answer = await AnswerRepository.find({ where: { } });
+      const answer = await AnswerRepository.find({ where: {} });
       return res.status(200).json({ message: `Answer retrieved successfully: ${JSON.stringify(answer)}` });
     } catch (error) {
       return res.status(500).json({ error: 'Failed to fetch answers' });
@@ -31,7 +42,7 @@ export class AdminAnswerController {
 
   async readAnswer(req: Request, res: Response) {
     try {
-      const answerId  = req.params.answerId;
+      const answerId = req.params.answerId;
       if (!answerId) {
         return res.status(400).json({ error: 'Answer ID is required' });
       }
@@ -45,7 +56,7 @@ export class AdminAnswerController {
 
   async updateAnswer(req: Request<{}, {}, UpdateAnswerInput>, res: Response) {
     try {
-      
+
       return res.status(200).json({ message: 'Answer updated successfully' });
     } catch (error) {
       return res.status(500).json({ error: 'Failed to update answer' });
@@ -54,7 +65,7 @@ export class AdminAnswerController {
 
   async deleteAnswer(req: Request, res: Response) {
     try {
-      
+
       return res.status(200).json({ message: 'Answer deleted successfully' });
     } catch (error) {
       return res.status(500).json({ error: 'Failed to delete answer' });
