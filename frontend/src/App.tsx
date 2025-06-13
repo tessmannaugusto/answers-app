65611794efd8a33d1807bcaa8bde214bde75fc72
Augusto Yunes <augusto.yunes@accenture.com>
2025-06-13 19:26:03 -0300

    started fixing css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { Container } from '@mui/material';
+import { Container, Box } from '@mui/material';
 import { Login } from './components/Login';
 import { ProtectedRoute } from './components/ProtectedRoute';
 import { Register } from './components/Register';
@@ -9,22 +9,28 @@ import { Header } from './components/Header';
 function App() {
   return (
     <Router>
-      <Header />
-      <Container>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="/home"
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="*" element={<Navigate to="/home" />} />
-        </Routes>
-      </Container>
+      <Box sx={{ 
+        display: 'flex', 
+        flexDirection: 'column',
+        minHeight: '100vh'
+      }}>
+        <Header />
+        <Container maxWidth={false} sx={{ flexGrow: 1, p: 3 }}>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/home"
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="*" element={<Navigate to="/home" />} />
+          </Routes>
+        </Container>
+      </Box>
     </Router>
   );
 }
